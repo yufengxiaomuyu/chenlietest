1c9d45ef047dab0b102547a2833f93af40ed5069
yufengxiaomuyu <838983866@qq.com>
2019-01-28 17:59:40 +0800

    分数页结构样式已完成

--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -1,65 +1,61 @@
 // pages/circle/circle.js
 Page({
   data: {
-    progress_txt: '正在匹配中...',
     count: 0, // 设置 计数器 初始为0
-    countTimer: null, // 设置 定时器 初始为null
-    score: 0
+    countTimer: '', // 设置 定时器 初始为null
+    width: 104,
+    r: 98,
+    score: 8,
   },
   onLoad: function (options) {
-    let step = (this.data.score / 100) * 2
+    let circumference = 2 * this.data.r * Math.PI
+    let offset = (1 - this.data.score / 100) * circumference
+    this.setData({
+      circumference: circumference,
+      offset: offset
+    })
     this.drawProgressbg()
-    this.drawCircle(step)
-    // this.countInterval()
+    this.countInterval(offset)
   },
-  // onReady: function () {
-  //   this.drawProgressbg()
-  // },
   drawProgressbg: function () {
     // 使用 wx.createContext 获取绘图上下文 context
     var ctx = wx.createCanvasContext('canvasProgressbg')
-    ctx.arc(104, 104, 98, 0, 2 * Math.PI, false);
+    ctx.arc(this.data.width, this.data.width, this.data.r, 0, 2 * Math.PI, false);
     ctx.setLineWidth(8);// 设置圆环的宽度
     ctx.setStrokeStyle('#fbedb8'); // 设置圆环的颜色
     ctx.setLineCap('round') // 设置圆环端点的形状
-    // ctx.beginPath();//开始一个新的路径
     ctx.stroke();//对当前路径进行描边
     ctx.draw();
   },
-  drawCircle: function (step) {
-    let r = 104;
-    let circumference = 2 * r * Math.PI
-    let offset = circumference - ((this.data.score / 100) * circumference)
+  drawCircle: function (offset) {
     var context = wx.createCanvasContext('canvasProgress');
     context.setLineWidth(12);
     context.setStrokeStyle('#fbda43');
-    context.setLineCap('butt')
-    // context.beginPath();
-    // 参数step 为绘制的圆环周长，从0到2为一周 。 -Math.PI / 2 将起始角设在12点钟位置 ，结束角 通过改变 step 的值确定
-    context.arc(r, r, 98, -Math.PI / 2, 2 * Math.PI - Math.PI / 2, false);
-    context.setLineDash([circumference/2, circumference/2], 10)
+    context.setLineCap('round')
+    context.arc(this.data.width, this.data.width, this.data.r, - (Math.PI / 2), - (Math.PI / 2) + (2 * Math.PI), false);
+    context.setLineDash([this.data.circumference, this.data.circumference], offset)
     context.stroke()
     context.draw()
   },
-  countInterval: function () {
+  countInterval: function (offset) {
     // 设置倒计时 定时器 每100毫秒执行一次，计数器count+1 ,耗时6秒绘一圈
-    this.countTimer = setInterval(() => {
-      if (this.data.count <= 60) {
-        /* 绘制彩色圆环进度条  
-        注意此处 传参 step 取值范围是0到2，
-        所以 计数器 最大值 60 对应 2 做处理，计数器count=60的时候step=2
-        */
-        this.drawCircle(this.data.count / (60 / 2))
-        this.data.count++;
-      } else {
-        this.setData({
-          progress_txt: "匹配成功"
-        });
-        clearInterval(this.countTimer);
+    console.log(offset)
+    let curOffset = this.data.circumference
+    let step = (curOffset - offset) * 16 / 1000
+    let count = 0 //计数器
+    let interval//定时器
+    interval = setInterval(() => {
+      curOffset = curOffset - step
+      console.log(curOffset)
+      this.drawCircle(curOffset)
+      count++
+      console.log(count)
+      if (count >= Math.floor(1000/16)) {
+        curOffset = offset
+        console.log(curOffset)
+        this.drawCircle(curOffset)
+        clearInterval(interval);
       }
-    }, 100)
+    }, 16);
   },
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/score/score.js
+++ pages/score/score.js
@@ -5,62 +5,67 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    count: 0, // 设置 计数器 初始为0
+    countTimer: '', // 设置 定时器 初始为null
+    width: 104,
+    r: 98,
+    score: 90,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let circumference = 2 * this.data.r * Math.PI
+    let offset = (1 - this.data.score / 100) * circumference
+    this.setData({
+      circumference: circumference,
+      offset: offset
+    })
+    this.drawProgressbg()
+    this.countInterval(offset)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  drawProgressbg: function() {
+    // 使用 wx.createContext 获取绘图上下文 context
+    var ctx = wx.createCanvasContext('canvasProgressbg')
+    ctx.arc(this.data.width, this.data.width, this.data.r, 0, 2 * Math.PI, false);
+    ctx.setLineWidth(8); // 设置圆环的宽度
+    ctx.setStrokeStyle('#fbedb8'); // 设置圆环的颜色
+    ctx.setLineCap('round') // 设置圆环端点的形状
+    ctx.stroke(); //对当前路径进行描边
+    ctx.draw();
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  drawCircle: function(offset) {
+    var context = wx.createCanvasContext('canvasProgress');
+    context.setLineWidth(12);
+    context.setStrokeStyle('#fbda43');
+    context.setLineCap('round')
+    context.arc(this.data.width, this.data.width, this.data.r, -(Math.PI / 2), -(Math.PI / 2) + (2 * Math.PI), false);
+    context.setLineDash([this.data.circumference, this.data.circumference], offset)
+    context.stroke()
+    context.draw()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  countInterval: function(offset) {
+    // 设置倒计时 定时器 每100毫秒执行一次，计数器count+1 ,耗时6秒绘一圈
+    console.log(offset)
+    let curOffset = this.data.circumference
+    let step = (curOffset - offset) * 16 / 1000
+    let count = 0 //计数器
+    let interval //定时器
+    interval = setInterval(() => {
+      curOffset = curOffset - step
+      console.log(curOffset)
+      this.drawCircle(curOffset)
+      count++
+      console.log(count)
+      if (count >= Math.floor(1000 / 16)) {
+        curOffset = offset
+        console.log(curOffset)
+        this.drawCircle(curOffset)
+        clearInterval(interval);
+      }
+    }, 16);
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(res) {
+    return {
+      title: '陈列测评',
+      path: '/pages/index/index'
+    }
   }
 })
